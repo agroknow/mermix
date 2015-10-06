804297687ddcb5d2e49cb90be51d5977b3773ef6
VTsantilis <vtsantilis@gmail.com>
2015-10-06 19:59:37 +0300

    move tips script to theme

--- sites/all/modules/ak_mermix_tools/mermix_tools_tips.js
+++
@@ -1,6 +0,0 @@
-jQuery('.description').each(function(){
-	var $description = jQuery(this);
-	var $target = jQuery(this).closest('.form-wrapper');
-	$description.hide();
-	new Opentip($target, $description.text(),'',{target:true,tipJoint:'right'});
-	});
--- sites/default/themes/realia/js/realia.js
+++ sites/default/themes/realia/js/realia.js
@@ -65,4 +65,11 @@ Drupal.behaviors.languageSwitch = {
         }
     }
 }
-
+jQuery(document).ready(function(){
+jQuery('.description').each(function(){
+	var $description = jQuery(this);
+	var $target = jQuery(this).closest('.form-wrapper');
+	$description.hide();
+	new Opentip($target, $description.text(),'',{target:true,tipJoint:'right'});
+	});
+});
