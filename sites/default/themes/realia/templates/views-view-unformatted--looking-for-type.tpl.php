aeed0918cc50663fb32c85e3b95d069fa02053ae
VTsantilis <vtsantilis@gmail.com>
2015-10-21 13:44:30 +0300

    add template for view

---
+++ sites/default/themes/realia/templates/views-view-unformatted--looking-for-type.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php $typeid = arg(1) && is_numeric(arg(1)) ? arg(1) : '' ?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+    <div><a class="btn btn-primary" href="<?php echo user_is_logged_in() ? url('node/add/apartment/' . $typeid) : url('machinery-add/' . $typeid) ?>"><?php print t('Add it') ?></a></div>
+  </div>
+<?php endforeach; ?>
