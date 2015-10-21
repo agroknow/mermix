3a0796587984430ecd3ebe1b2133bf0e8e319ab4
VTsantilis <vtsantilis@gmail.com>
2015-10-21 13:44:17 +0300

    add js to toggle banner

---
+++ sites/all/modules/ak_mermix_tools/mermix_tools_banner.js
@@ -0,0 +1,17 @@
+(function ($) {
+  Drupal.behaviors.akMermixTools = {
+    attach: function (context, settings) {
+	var banner = $('.banner-container');
+	if($.cookie('banner-closed') == '1') {
+	    //hide
+	    banner.hide();
+	} else {
+	    banner.fadeIn();
+	}
+    $('.banner-container', context).click(function () {
+      $.cookie('banner-closed','1');
+      banner.fadeOut();
+    });
+    }
+  };
+})(jQuery);
