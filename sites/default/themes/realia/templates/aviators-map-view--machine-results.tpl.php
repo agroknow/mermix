c795b93f633bc0fea3395f83b59583bb856408be
VTsantilis <vtsantilis@gmail.com>
2015-10-21 19:19:35 +0300

    add pagination(scrolling) in search results

--- sites/default/themes/realia/templates/aviators-map-view--machine-results.tpl.php
+++ sites/default/themes/realia/templates/aviators-map-view--machine-results.tpl.php
@@ -6,6 +6,10 @@
  * @ingroup views_templates
  */
 ?>
+<?php 
+drupal_add_js(drupal_get_path('module', 'ak_mermix_tools') . '/ak_mermix_scrolling.js');
+$pagination = array(); 
+?>
 <div class="row">
     <div class="span7">
 	<div id="<?php print $view_id ?>" style="width:<?php print $width; ?>; height:<?php print $height; ?>;"></div>
@@ -15,9 +19,18 @@
     	<h3><?php print $title; ?></h3>
 	<?php endif; ?>
 	<?php foreach ($rows as $id => $row): ?>
-    	<div class="result-item">
+	<div <?php if( $id%6 == 0 ) {print 'id="position-'.$id.'"'; $pagination[] = $id; } ?> class="result-item">
     	    <span class="number"><?php print $id + 1 ?></span><?php print $row; ?>
     	</div>
 	<?php endforeach; ?>
+    </div>
+</div>
+<div class="row">
+    <div class="span12">
+	<div id="results-pagination">
+	<?php foreach($pagination as $key => $pagenum) { ?>
+	    <a href="#" data-elem="<?php echo $pagenum ?>" class="<?php echo $key == 0 ? 'active':'' ?>"><?php echo $key+1 ?></a>
+	<?php } ?>
+	</div>
     </div>
 </div>
