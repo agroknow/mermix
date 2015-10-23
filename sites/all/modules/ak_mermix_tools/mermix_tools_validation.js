f4f23bd3e27a5cacccb4fe86523d327c275a3bce
VTsantilis <vtsantilis@gmail.com>
2015-10-23 22:10:14 +0300

    add validation rules

--- sites/all/modules/ak_mermix_tools/mermix_tools_validation.js
+++ sites/all/modules/ak_mermix_tools/mermix_tools_validation.js
@@ -1,8 +1,14 @@
+jQuery.validator.setDefaults({ ignore: ":hidden:not(select)" }); //for all select
 jQuery("#machinery-search-form").validate({
   rules: {
     'date_to[date]': {
     required: function(element){
             return jQuery("#edit-date-from-datepicker-popup-0").val()!="";
+        }
+    },
+    'date_from[date]': {
+    required: function(element){
+            return jQuery("#edit-date-to-datepicker-popup-0").val()!="";
         }
     }
   }
@@ -13,6 +19,28 @@ jQuery(".webform-client-form").validate({
     required: function(element){
             return jQuery("#edit-submitted-date-from-datepicker-popup-0").val()!="";
         }
+    },
+    'submitted[date_from][date]': {
+    required: function(element){
+            return jQuery("#edit-submitted-date-to-datepicker-popup-0").val()!="";
+        }
+    },
+    'submitted[type]' : {
+	required: true
+    }
+  }
+});
+
+jQuery("#machinery-add-form").validate({
+  rules: {
+    machine_name: {
+    required: true
+    },
+    place: {
+    required: true
+    },
+    'machine_cultivation[]': {
+    required: true
     }
   }
 });
