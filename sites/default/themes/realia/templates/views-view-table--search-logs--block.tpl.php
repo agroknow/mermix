f98ab54444b7bc06955c75bc5e27493fc1528f1f
VTsantilis <vtsantilis@gmail.com>
2015-11-03 06:34:12 +0200

    customize display for search logs displayed on user

---
+++ sites/default/themes/realia/templates/views-view-table--search-logs--block.tpl.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $header: An array of header labels keyed by field id.
+ * - $caption: The caption for this table. May be empty.
+ * - $header_classes: An array of header classes keyed by field id.
+ * - $fields: An array of CSS IDs to use for each field id.
+ * - $classes: A class or classes to apply to the table, based on settings.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are keyed by row number, fields within rows are keyed by field ID.
+ * - $field_classes: An array of classes to apply to each field, indexed by
+ *   field id, then row number. This matches the index in $rows.
+ * @ingroup views_templates
+ */
+?>
+<table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
+   <?php if (!empty($title) || !empty($caption)) : ?>
+     <caption><?php print $caption . $title; ?></caption>
+  <?php endif; ?>
+  <?php if (!empty($header)) : ?>
+    <thead>
+      <tr>
+	  <?php 
+	  unset($header['coords']); 
+	  unset($header['machine_type']); 
+	  ?>
+        <?php foreach ($header as $field => $label): ?>
+          <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
+            <?php print $label; ?>
+          </th>
+        <?php endforeach; ?>
+	  <th> Search url </th>
+      </tr>
+    </thead>
+  <?php endif; ?>
+  <tbody>
+    <?php foreach ($rows as $row_count => $row): ?>
+      <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
+          <?php foreach ($row as $field => $content): ?>
+	  <?php 
+	  $path = $row['coords'] ? $row['coords'] . '/' : 'all/' ;
+	  $path .= $row['machine_type'] ? $row['machine_type'] . '/' : 'all/' ;
+	  $path .= $row['date_from'] ? date('Y-m-d', strtotime($row['date_from'])) . '--' . date('Y-m-d', strtotime($row['date_to'])) . '/' : 'all/' ;
+	  if($field == 'coords' || $field == 'machine_type' ) {
+	      continue;
+	  }
+	  ?>
+	  <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+            <?php print $content; ?>
+          </td>
+        <?php endforeach; ?>
+	  <td><?php print l('search again','machine-results/' . $path) ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
