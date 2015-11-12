7b275dcfbf4713dcd407ac3e193c54db386ffa86
VTsantilis <vtsantilis@gmail.com>
2015-11-12 22:30:38 +0200

    add script for getting user location

---
+++ sites/all/modules/ak_mermix_tools/js/user_location.js
@@ -0,0 +1,30 @@
+function geolocation(cookie) {
+    if (typeof cookie === 'undefined') cookie = true;
+    if (navigator.geolocation) {
+    var startPos;
+    var geoOptions = {
+    	maximumAge: 5 * 60 * 1000,
+    }
+
+    var geoSuccess = function(position) {
+      startPos = position;
+      if(cookie) {
+        console.log('cookie');
+        jQuery.cookie("geolocation",startPos.coords.latitude +","+startPos.coords.longitude, { path: '/' });
+      } else {
+        console.log('redirect');
+        window.location.href = 'machine-results/' + startPos.coords.latitude +","+startPos.coords.longitude;
+      }
+    };
+    var geoError = function(error) {
+      console.log('Error occurred. Error code: ' + error.code);
+      // error.code can be:
+      //   0: unknown error
+      //   1: permission denied
+      //   2: position unavailable (error response from location provider)
+      //   3: timed out
+    };
+
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+    } 
+}
