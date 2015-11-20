9e060be45934d680b156b7f0b1b2e120ced6d2c5
VTsantilis <vtsantilis@gmail.com>
2015-11-20 05:34:19 +0200

    add class to view tpl

--- sites/default/themes/realia/templates/owlcarousel-views--machine-types.tpl.php
+++ sites/default/themes/realia/templates/owlcarousel-views--machine-types.tpl.php
@@ -10,13 +10,13 @@ if ($view->current_display == 'block_promo') {
     foreach ($groupedItems as $key => $item) {
 	$colClass = ($key % 2 == 0) ? 'odd' : 'even';
 	$final[]['row'] = isset($item[1]) ?
-		'<div class="sub-item ' . $colClass . ' odd">' . $item[0]['row'] . '</div><div class="sub-item ' . $colClass . ' even">' . $item[1]['row'] . '</div>' :
-		'<div class="sub-item ' . $colClass . ' odd">' . $item[0]['row'] . '</div>';
+		'<div class="sub-item after ' . $colClass . ' odd">' . $item[0]['row'] . '</div><div class="sub-item after ' . $colClass . ' even">' . $item[1]['row'] . '</div>' :
+		'<div class="sub-item after ' . $colClass . ' odd">' . $item[0]['row'] . '</div>';
     }
     print theme('owlcarousel', array('items' => $final, 'settings' => $settings));
 } else {
     foreach ($items as $key => $item) {
-	$final[]['row'] = '<div class="sub-item single">' . $item['row'] . '</div>';
+	$final[]['row'] = '<div class="sub-item after single">' . $item['row'] . '</div>';
     }
     print theme('owlcarousel', array('items' => $final, 'settings' => $settings));
 }
